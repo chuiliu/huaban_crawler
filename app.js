44c6090a5fbae0761c19843ac205955244fc5659
chuiliu <zhengchuiliu@foxmail.com>
2016-06-05 23:39:39 +0800

    update

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 var http = require('http');
 var fs = require('fs');
+var async = require('async');
 
-var url = 'http://huaban.com/boards/25498000/?iwlq03e7&max=580817693&limit=100&wfl=1';
-// var url = 'http://huaban.com/boards/33147249/?iwkskixs&max=930057654&limit=20&wfl=1';
+
+var url = 'http://huaban.com/boards/25498000/?iwlq03e8&max=780917693&limit=101&wfl=1';  // 宫崎骏
+// var url = 'http://huaban.com/boards/2502601/?ip2ptxup&max=31059796&limit=70&wfl=1';     // 动物
 var imageUrlBase = 'http://img.hb.aicdn.com/';
+var downloadPath = 'download/';
 
 // 获取花瓣网数据
 var fetchData = function(html) {
+
+    console.log('开始抓取花瓣网数据');
 
     // 取到画板数据
     var board = /(app\.page\["board"\]).*};/.exec(html)[0];
@@ -15,40 +20,58 @@ var fetchData = function(html) {
 
     // 画板照片数量
     var count = board.pin_count;
-    console.log('照片数量: ' + count);
+    console.log('画板照片数量: ' + count);
     // 照片
     // console.log(board)
-    // console.log(board.pins);
+    console.log(board.pins);
     console.log('即将下载' + board.pins.length + '张图片');
 
     // 创建文件夹
-    var downloadPath = 'download/' + board.board_id + '/';
+    downloadPath += board.board_id + '/';
     if(!fs.existsSync(downloadPath)) {
          fs.mkdirSync(downloadPath);
     }
 
-    var i = 0;
-    board.pins.forEach(function(item, index) {
-        var imgUrl =  imageUrlBase + item.file.key;
-        var types = {
-            'image/png': '.png',
-            'image/jpeg': '.jpg',
-            'image/gif': '.gif'
-        };
-        var filename = item.file.id + (types[item.type] || '.jpg');
-        var ws = fs.createWriteStream(downloadPath + filename);
+    async.mapLimit(board.pins, 3, function(item, callback) {
+            var imgUrl =  imageUrlBase + item.file.key;
+            var types = {
+                'image/png': '.png',
+                'image/jpeg': '.jpg',
+                'image/bmp': '.bmp',
+                'image/gif': '.gif'
+            };
+            console.log(item.file.type);
+            var filename = item.file.id + (types[item.file.type] || '.jpg');
+
+            download(imgUrl, filename, callback);
+        }, function (err, result) {
+            console.log('完成情况：' + result);
+        }
+    );
+};
+
+
+// 下载
+var download = function(imgUrl, filename, callback) {
+    var filePath = downloadPath + filename;
+    if (fs.existsSync(filePath)) {
+        console.log('文件 ' + filePath + ' 已存在');
+        callback(null, '文件已存在');
+    } else {
+        var ws = fs.createWriteStream(filePath);
         ws.on('finish', function() {
-            console.log('已下载：'+ (++i) + '张, ' + filename + ' 下载完成');
+            console.log('' + filename + ' 已下载');
+            callback(null, filename + '下载成功');
         });
 
         http.get(imgUrl, function(res) {
             res.pipe(ws);
+        }).on('finish', function() {
+            console.log('请求完成: ', imgUrl);
         }).on('error', function() {
             console.log('error');
         });
-    });
-
-
+    }
 };
 
 
@@ -63,7 +86,6 @@ http.get(url, function(res) {
     });
 
     res.on('end', function() {
-        // console.log(html);
         fetchData(html);
     });
 }).on('error', function() {
